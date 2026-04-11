eff454ffff2a1f37b638f8db8f33d52dad4c1c5e
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2026-04-11 09:32:14 -0700

    render change

---
+++ Assets/Scripts/ChangeShipVisibility.cs
@@ -0,0 +1,28 @@
+using Unity.Hierarchy;
+using UnityEngine;
+
+public class ChangeShipVisibility : MonoBehaviour
+{
+    private bool viewingTop = true;
+    [SerializeField] private GameObject topHalf;
+    [SerializeField] private GameObject bottomHalf;
+    [SerializeField] private Renderer topRenderer;
+
+    [SerializeField] private float transparentAlpha;
+    [SerializeField] private float opaqueAlpha;
+
+    public void ToggleVisibility() 
+    {
+        viewingTop = !viewingTop;
+        if (viewingTop)
+        {
+            topRenderer.material.color = new Color(topRenderer.material.color.r,
+                topRenderer.material.color.g, topRenderer.material.color.b, transparentAlpha);
+        }
+        else 
+        {
+            topRenderer.material.color = new Color(topRenderer.material.color.r,
+                 topRenderer.material.color.g, topRenderer.material.color.b, opaqueAlpha);
+        }
+    }
+}
