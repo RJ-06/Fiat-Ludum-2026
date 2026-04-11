644d65b8ef29406e48ce9a6d346a7c3c4facddff
RJ-06 <54755468+RJ-06@users.noreply.github.com>
2026-04-11 11:09:59 -0700

    task list

---
+++ Assets/Scripts/TaskList.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TaskList", menuName = "Scriptable Objects/TaskList")]
+public class TaskList : ScriptableObject
+{
+    public List<Task> tasks = new List<Task>();
+
+    public Task? GetTaskByID(int taskID) 
+    {
+        if (taskID < 0 || taskID >= tasks.Count) return null;
+
+        return tasks[taskID];
+
+    }
+    
+}
+
+[Serializable]
+public struct Task
+{
+    public string name;
+    public string description;
+    public float fixTime;
+    public float fixRate;
+    public float activeTimer;
+}
---
+++ Assets/Scripts/TaskManager.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+    [SerializeField] private TaskList taskList;
+    public List<Task> activeTasks = new List<Task>();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddTask() 
+    {
+        int selectedTask = Random.Range(0, taskList.tasks.Count);
+        activeTasks.Add(taskList.tasks[selectedTask]);
+    }
+}
