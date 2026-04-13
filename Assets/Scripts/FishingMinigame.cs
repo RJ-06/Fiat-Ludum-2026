7d8635bebeb4ff44bf8f72304c089b832320c764
DaveyNavy <davidyzhu2@gmail.com>
2026-04-13 12:19:14 -0700

    Fix fish minigame and UI

--- Assets/Scripts/FishingMinigame.cs
+++ Assets/Scripts/FishingMinigame.cs
@@ -17,9 +17,13 @@ public class FishingMinigame : MonoBehaviour
     [SerializeField] Image failedCatchNotification;
 
     public TMPro.TMP_Text resultText;
+
     public enum ResourceType { Gold, Wood }
 
-    bool hasEnded = false;  
+    private bool hasEnded = false;
+
+    // ----------------- SCORING -----------------
+
     float GetThreshold(int level)
     {
         float baseThreshold = 0.5f;
@@ -32,16 +36,15 @@ public class FishingMinigame : MonoBehaviour
         float t = level / 4f;
         return Mathf.RoundToInt(Mathf.Lerp(min, max, t));
     }
+
     public (ResourceType type, int amount) GetReward(float score, int level)
     {
         float threshold = GetThreshold(level);
 
-        // Fail = no reward
         if (score < threshold)
             return (ResourceType.Gold, 0);
 
-        // Pick random resource
-        ResourceType type = (ResourceType)Random.Range(0, 3);
+        ResourceType type = (ResourceType)Random.Range(0, 2);
 
         int maxAmount = 0;
 
@@ -57,119 +60,128 @@ public class FishingMinigame : MonoBehaviour
         }
 
         float scoreFactor = Mathf.InverseLerp(threshold, 1f, score);
-
         int finalAmount = Mathf.RoundToInt(maxAmount * scoreFactor);
 
         return (type, finalAmount);
     }
+
+    // ----------------- UPDATE -----------------
+
     public void FixedUpdate()
     {
-        if (canCatch) 
+        if (canCatch)
         {
             timer += Time.fixedDeltaTime;
         }
-        if (!GameplayModeManager.Instance.isFishingMode()) 
+
+        if (!GameplayModeManager.Instance.isFishingMode())
         {
+            waitingForFishNotification.enabled = false;
             canCatchNotification.enabled = false;
             failedCatchNotification.enabled = false;
-            waitingForFishNotification.enabled = false;
-            if (!GameplayModeManager.Instance.isFishingMode())
-            {
-                canCatchNotification.enabled = false;
-                failedCatchNotification.enabled = false;
-                waitingForFishNotification.enabled = false;
-            }
         }
     }
+
+    // ----------------- START FISHING -----------------
 
     public IEnumerator StartFishing()
     {
         score = 0;
         timer = 0;
         hasEnded = false;
+        canCatch = false;
+
+        waitingForFishNotification.enabled = true;
         canCatchNotification.enabled = false;
         failedCatchNotification.enabled = false;
-        waitingForFishNotification.enabled = true;
-        canCatch = false;
-        yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
-        canCatchNotification.enabled = true;
-        canCatch = true;
-        StartCoroutine(StopFishing());
 
+        yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+
+        waitingForFishNotification.enabled = false;
+        canCatchNotification.enabled = true;
+
+        canCatch = true;
+
+        StartCoroutine(StopFishing());
     }
+
+    // ----------------- AUTO FAIL TIMER -----------------
 
     public IEnumerator StopFishing()
     {
-       
         yield return new WaitForSeconds(timeToCatch);
-        if (canCatch) 
-        {
-            yield return null;
-        }
 
         if (hasEnded) yield break;
 
         hasEnded = true;
-        canCatchNotification.enabled = false;
+
         canCatch = false;
         timer = 0;
-        if(score == 0)failedCatchNotification.enabled = true;
+
+        failedCatchNotification.enabled = true;
+
         yield return new WaitForSeconds(1f);
-        canCatchNotification.enabled = false;
+
         failedCatchNotification.enabled = false;
+        canCatchNotification.enabled = false;
         waitingForFishNotification.enabled = false;
+
+        GameplayModeManager.Instance.SetFishingMode(false);
     }
+
+    // ----------------- PLAYER INPUT -----------------
 
     public void tryToCatch()
     {
         if (hasEnded) return;
 
         hasEnded = true;
+
         if (canCatch)
         {
-            canCatch = false;
-            canCatchNotification.enabled = false;
             score = 1 - timer / timeToCatch;
-            timer = 0;
-            StartCoroutine(ShowResultAndExit());
-
-            canCatchNotification.enabled = false;
-            failedCatchNotification.enabled = false;
-            waitingForFishNotification.enabled = false;
         }
-        if (!canCatch) 
+        else
         {
-            canCatchNotification.enabled = false;
-            failedCatchNotification.enabled = false;
-            waitingForFishNotification.enabled = false;
-            StartCoroutine(ShowResultAndExit());
-            timer = 0;
             score = 0;
         }
+
+        canCatch = false;
+        timer = 0;
+
+        StartCoroutine(ShowResultAndExit());
     }
+
+    // ----------------- RESULT FLOW -----------------
+
     IEnumerator ShowResultAndExit()
     {
-        (ResourceType type, int amount) reward = GetReward(score, ShipManager.shipManager.fishingLevel);
+        var reward = GetReward(score, ShipManager.shipManager.fishingLevel);
+
         switch (reward.type)
         {
             case ResourceType.Gold:
                 ShipManager.shipManager.gold += reward.amount;
                 break;
+
             case ResourceType.Wood:
                 ShipManager.shipManager.shipHealth += reward.amount;
-                ShipManager.shipManager.shipHealth = Mathf.Clamp(ShipManager.shipManager.shipHealth, 0, 100);
+                ShipManager.shipManager.shipHealth =
+                    Mathf.Clamp(ShipManager.shipManager.shipHealth, 0, 100);
                 break;
         }
+        canCatchNotification.enabled = false;
 
-        resultText.text = $"Your score was {score}. You caught {(reward.type == ResourceType.Gold ? "Gold" : "Wood")} x{reward.amount}!";
-        // Show result text for 2 seconds
+        resultText.text =
+            $"Your score was {score:F2}. You caught {reward.type} x{reward.amount}!";
+
         yield return new WaitForSeconds(2f);
 
         resultText.text = "";
 
+        waitingForFishNotification.enabled = false;
         canCatchNotification.enabled = false;
         failedCatchNotification.enabled = false;
-        waitingForFishNotification.enabled = false;
 
         GameplayModeManager.Instance.SetFishingMode(false);
     }
