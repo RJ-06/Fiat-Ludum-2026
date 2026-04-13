4bfadb4676a0edcd7a72bd08f261d7bccb4435d6
JoshuaCVillanova <joshuacvillanova@gmail.com>
2026-04-13 01:08:30 -0700

    add iceberg collision sound

--- Assets/Scripts/CollisionWithIceberg.cs
+++ Assets/Scripts/CollisionWithIceberg.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class CollisionWithIceberg : MonoBehaviour
 {
+    [SerializeField] AudioSource icebergHitSound;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Iceberg"))
+        {
+            icebergHitSound.Play();
             Debug.Log("Ship hit iceberg!");
+        }
     }
 }
